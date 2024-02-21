929ea4437c05b2618c1a1e5e81dc74dca4729e41
Chara Anis <chara@et.esiea.fr>
2024-02-21 22:20:27 +0100

    focntionnement global fini

--- sone.js
+++ sone.js
@@ -2,27 +2,47 @@
 
 const http = require("http");
 const url = require("url");
+const statique = require("./req_statique");
 
 let sone = {};
 sone.listRequete = [];
+sone.listNameRequete = [];
 
 sone.requete = function (req, res, query, pathname)
 {
+    //IDENTIFIE LA BONNE REQUETE 
     for (let i = 0; i < sone.listRequete.length; i++)
     {
-        if (pathname === sone.listRequete[i])
+        if (pathname === sone.listNameRequete[i])
         {
-            sone.listRequete[i-1](req, res, query);
+            try {
+            sone.listRequete[i](req, res, query);
+            } catch (e) {
+                
+            console.log('Erreur : ' + e.stack);
+            console.log('Erreur : ' + e.message);
+            // console.trace();
+            const show_erreur = function (req, res, query) {
+                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.write('ERREUR SERVEUR');
+                res.end();
+            };
+            show_erreur(req, res, query);            
+            }return;
         }
-    }    
-}
+    }
 
+    //REQ_STATIQUE
+    statique(req, res, query);
+}
+//ENREGISTRE LES REQUETES
 sone.addRequete = function (requete, name_requete)
 {
     sone.listRequete.push(requete);
-    sone.listRequete.push(name_requete);
+    sone.listNameRequete.push(name_requete);
 }
 
+//EXECUTE LE SERVEUR
 sone.run = function (port, sone)
 {
     const serv = function (req, res)
